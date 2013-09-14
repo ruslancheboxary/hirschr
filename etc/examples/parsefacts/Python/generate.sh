571b49d84ac6aee3abdd0568373b460cb9929f3b
katspaugh <katspaugh@yandex-team.ru>
2013-09-14 23:25:12 +0400

    rm tomita

--- etc/examples/parsefacts/Python/generate.sh
+++
@@ -1,2 +0,0 @@
-protoc --proto_path=.. --python_out=. ../facts.proto
-
--- etc/examples/tutorial5/main.cxx
+++
@@ -1,8 +0,0 @@
-#encoding "utf-8"
-#include <date.cxx>
-
-Animal -> Noun<kwtype=animal>;
-WithWho -> 'у' (Adj<gnc-agr[1]>) Animal<gram='род', rt, gnc-agr[1]> interp (Sparrow.Who);
-WithWho -> 'с' (Adj<gnc-agr[1]>) Animal<gram='твор', rt, gnc-agr[1]> interp (Sparrow.Who);
-S -> Date interp (Sparrow.When) WithWho;
-S -> WithWho Date interp (Sparrow.When);
